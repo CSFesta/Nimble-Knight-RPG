d808f12f683637e354c658f53b2fe2d91697b725
CSFesta <cauafesta@hotmail.com>
2026-02-19 23:58:12 -0300

    Fix health on enemyBase

--- Cenas/Enemies/health.gd
+++ Cenas/Enemies/health.gd
@@ -1,26 +1,18 @@
 extends Node
 
-@export var max_life: int = 10
+var max_life: int
 var life: int
 
 signal died
 signal damaged(amount)
 
-func _ready() -> void:
-	life = max_life
+func setup(hp: int) -> void:
+	max_life = hp
+	life = hp
 
 func take_damage(amount: int) -> void:
-	print("Health.take_damage chamado | Dano:", amount, " | Vida antes:", life)
-
 	life = max(life - amount, 0)
 	damaged.emit(amount)
-
-	print("Vida depois:", life)
-
+	
 	if life == 0:
-		print("Health.die emitido")
-		die()
-
-
-func die() -> void:
-	died.emit()
+		died.emit()
